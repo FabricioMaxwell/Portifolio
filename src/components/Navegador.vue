8b99daedef1883668d7053d659ff69f3fb29e5af
Fabrício Maxwell <102274424+FabricioMaxwell@users.noreply.github.com>
2024-08-23 17:21:15 -0300

    Criação do app

---
+++ src/components/Navegador.vue
@@ -0,0 +1,46 @@
+<script setup>
+import MenuIcon from '/node_modules/vue-material-design-icons/Menu.vue'
+</script>
+
+<template>
+
+    <nav class="nav">
+        <span><a href="#">Fabrício Maxwell</a></span>
+        <ul class="nav-list">
+            <li><a href="#">Sobre</a></li>
+            <li><a href="">Experiência</a></li>
+            <li><a href="">Projetos</a></li>
+            <li><a href="">Contato</a></li>
+        </ul>
+        <MenuIcon :size="35" />
+    </nav>
+</template>
+
+<style scoped>
+.nav {
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+    align-items: center;
+    padding-inline: 40px;
+    position: relative;
+}
+
+.nav .nav-list {
+    display: flex;
+    position: absolute;
+    flex-direction: column;
+    gap: 0.8rem;
+    padding: 1rem;
+    top: 60px;
+    right: 44px;
+    background-color: white;
+    border-radius: 15px;
+    box-shadow: 10px 10px 15px gray;
+    
+
+
+}
+
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navegador from '../components/Navegador.vue'
+</script>
+
+<template>
+    <header>
+        <Navegador />
+    </header>
+</template>
+
+<style scoped>
+header {
+    position: relative;
+    height: 5rem;
+}
+
+</style>
